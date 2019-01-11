0304276ef4bfb441dc2acbd22305275d1596ed1f
CJ <cj84lafleur@gmail.com>
2019-01-11 11:43:43 -0500

    Replaced arrays with queues in order to prepare to multiprocess.

--- NetScan.py
+++ NetScan.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 import socket
 from subprocess import Popen, PIPE
 
@@ -7,9 +8,9 @@ class Network:
     IPnetwork = ""
     StartIP = ""
     EndIP = ""
-    IPrange = []
-    ActiveIPs = []
-    Hostnames = []
+    IPrange = mp.Queue()
+    ActiveIPs = mp.Queue()
+    Hostnames = mp.Queue()
 
     # Get IP range
     def getiprange(self):
@@ -20,30 +21,32 @@ class Network:
         self.EndIP = int(self.EndIP)
         while self.StartIP <= self.EndIP:
             temp = str(self.StartIP)
-            self.IPrange.append(str(self.IPnetwork) + "." + temp)
+            self.IPrange.put(str(self.IPnetwork) + "." + temp)
             self.StartIP += 1
 
     # Get active hosts
     def gethosts(self):
-        for ip in self.IPrange:
-            ip = str(ip)
+        while not self.IPrange.empty():
+            ip = self.IPrange.get()
             toping = Popen(['ping', '-c', '1', '-i', '0.1', ip], stdout=PIPE)
             output = toping.communicate()[0]
             hostalive = toping.returncode
             if hostalive == 0:
-                self.ActiveIPs.append(ip)
+                self.ActiveIPs.put(ip)
 
     # Get host names
     def gethostnames(self):
-        for x in self.ActiveIPs:
-            self.Hostnames.append(socket.getfqdn(x))
+        while not self.ActiveIPs.empty():
+            ip = self.ActiveIPs.get()
+            hostn = socket.getfqdn(ip)
+            temp = hostn + "\t " + ip
+            self.Hostnames.put(temp)
 
     # Print active hosts
     def printhosts(self):
-        x = 0
-        while x < len(self.Hostnames):
-            print(self.ActiveIPs[x], " ", self.Hostnames[x])
-            x += 1
+        while not self.Hostnames.empty():
+            print(self.Hostnames.get())
+
 
 
 Scan: Network = Network()
