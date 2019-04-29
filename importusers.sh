1b1e127cd6c7876a91958c436574d222739f8512
CJ <cj84lafleur@gmail.com>
2019-04-29 11:48:03 -0400

    Fixed adding users to groups

--- importusers.sh
+++ importusers.sh
@@ -11,7 +11,7 @@ do
  f="$(echo $fullname | cut -f1 -d " ")"
  l="$(echo $fullname | cut -f2 -d " ")"
  gname="$(ipa group-find --gid=$gid | grep "Group name:" | cut -f 5 -d " ")"
- ipa user-add $uname --homedir=/mnt/nfs/users/$uname --first=$f --last=$l --displayname=$uname --shell=$shell --gidnumber=$gid
- ipa group-add-member  --users=$uname
+ ipa user-add $uname --homedir=/mnt/nfs/users/$uname --first=$f --last=$l --displayname=$uname --shell=$shell --gidnumber=$gid --noprivate
+ ipa group-add-member $gname --users=$uname 
 done < "$input"
 
