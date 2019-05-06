b0523b05844f5bd9ac974e51a15858731afccebb
CJ <cj84lafleur@gmail.com>
2019-05-06 12:57:02 -0400

    Final version

--- importusers.sh
+++ importusers.sh
@@ -11,7 +11,14 @@ do
  f="$(echo $fullname | cut -f1 -d " ")"
  l="$(echo $fullname | cut -f2 -d " ")"
  gname="$(ipa group-find --gid=$gid | grep "Group name:" | cut -f 5 -d " ")"
+ 
+ mkdir /mnt/nfs/users/$uname
  ipa user-add $uname --homedir=/mnt/nfs/users/$uname --first=$f --last=$l --displayname=$uname --shell=$shell --gidnumber=$gid --noprivate
- ipa group-add-member $gname --users=$uname 
-done < "$input"
+ ipa group-add-member $gname --users=$uname
+ mkdir /mnt/nfs/users/$uname/.ssh
+ ssh-keygen -t rsa -N "" -C "$uname@obelixmgmt" -f /mnt/nfs/users/$uname/.ssh/id_rsa
+ sshkey="$(cat /mnt/nfs/users/$uname/.ssh/id_rsa.pub)"
+ ipa user-mod $uname --sshpubkey="$(echo $sshkey)"
 
+ chown -R $uname /mnt/nfs/users/$uname
+done < "$input"
